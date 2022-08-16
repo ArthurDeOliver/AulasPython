703b3095035fd0fb3e67a23e4134abd93693eef7
Arthur de Oliveira <arthurdeoliveiraengen@gmail.com>
2022-08-16 14:28:35 -0300

    commit aula 13 e teste

---
+++ ExerciciosSlide/aula13.py
@@ -0,0 +1,25 @@
+'''l = [1,2,3,4,5,6,7,8,9,10]
+
+index = l.index(1,0,2)
+#encontra o índice de um elemento na lista primeiro parâmetro o elemento que deseja encontrar
+# os próximos 2 é o intervalo da busca através do índice
+
+print(index)'''
+
+produtos = {
+    'chocolate': 4.50,
+    'pão': 0.75,
+    'refrigerante': 6 
+}
+print('esses são os nossos produtos')
+for i in produtos.keys():
+    print(i)
+
+while True:
+    entrada = input('Qual o produto que deseja encontrar o preço? ')
+    
+    if(entrada == '00'):
+        print('programa encerrado')
+        break
+    if(entrada in produtos.keys()):
+        print('O produto {} tem o valor de R${}'.format(entrada, produtos[entrada]))
